544e3f5a5190ef14b18d271a69887e16fb0db5a9
mkgh25 <silverangel6211@gmail.com>
2025-03-02 03:28:49 +0000

    branched file

--- hello/Program.cs
+++ hello/Program.cs
@@ -4,9 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter a number: ");
-        var number = int.Parse(Console. ReadLine());
-
-        Console.WriteLine("This is Your number " + number);
+        Console.WriteLine("Done!");
     }
 }
